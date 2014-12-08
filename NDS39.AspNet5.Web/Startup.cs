b1ef0e01589d132d69bd3e1a0bd1d501e664445a
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-12-08 12:53:04 +0900

    startup "Hello world"

---
+++ NDS39.AspNet5.Web/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace NDS39.AspNet5.Web
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app)
+		{
+			app.Run(async context =>
+				{
+					var response = context.Response;
+					response.StatusCode = 200;
+					response.ContentType = "text/plain";
+					await response.WriteAsync("Hello world");
+				});
+		}
+	}
+}
