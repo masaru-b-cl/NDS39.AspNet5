4e619c3e7add3c509f30ceefdebfbcb2466f647b
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-12-08 12:56:43 +0900

    add inline middleware

--- NDS39.AspNet5.Web/Startup.cs
+++ NDS39.AspNet5.Web/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
+using System.Diagnostics;
 
 namespace NDS39.AspNet5.Web
 {
@@ -8,8 +9,17 @@ namespace NDS39.AspNet5.Web
 	{
 		public void Configure(IApplicationBuilder app)
 		{
+			app.Use(async (context, next) =>
+				{
+					Debug.WriteLine("@@@ start middleware @@@");
+					await next();
+					Debug.WriteLine("@@@ end middleware @@@");
+				});
+
 			app.Run(async context =>
 				{
+					Debug.WriteLine("@@@ run core app @@@");
+
 					var response = context.Response;
 					response.StatusCode = 200;
 					response.ContentType = "text/plain";
